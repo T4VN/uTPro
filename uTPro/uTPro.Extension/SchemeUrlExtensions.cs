07419b06707e2e841692d0080409f92e24a3d421
Nguyen Thien Tu <15050790+thientu995@users.noreply.github.com>
2025-12-10 11:00:18 +0700

    Update SchemeUrlExtensions.cs
    Signed-off-by: Nguyen Thien Tu <15050790+thientu995@users.noreply.github.com>

--- uTPro/uTPro.Extension/SchemeUrlExtensions.cs
+++ uTPro/uTPro.Extension/SchemeUrlExtensions.cs
@@ -1,10 +1,16 @@
-﻿namespace uTPro.Extension
+using Microsoft.AspNetCore.Http;
+
+namespace uTPro.Extension
 {
     public static class SchemeUrlExtensions
     {
         public static string AddScheme(string urlRedirect, string schemeDefault = "https")
         {
-            if (urlRedirect.StartsWith("/") || urlRedirect.StartsWith("http://") || urlRedirect.StartsWith("https://"))
+            if (urlRedirect.StartsWith("/"))
+            {
+                return schemeDefault + "://utpro.local" + urlRedirect;
+            }
+            if (urlRedirect.StartsWith("http://") || urlRedirect.StartsWith("https://"))
             {
                 return urlRedirect;
             }
