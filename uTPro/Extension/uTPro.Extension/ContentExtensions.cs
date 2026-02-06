88421736c7e9ebcfde39c3a2fb3883b82a29dfe1
Nguyen Thien Tu <15050790+thientu995@users.noreply.github.com>
2026-02-06 12:38:49 +0700

    Update Top/Bottom Component

---
+++ uTPro/Extension/uTPro.Extension/ContentExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.NetworkInformation;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using System.Linq;
+
+namespace uTPro.Extension
+{
+    public static class ContentExtensions
+    {
+        public static T? GetInherited<T>(this IPublishedContent content, string alias)
+        {
+            var node = content.AncestorsOrSelf()
+                .FirstOrDefault(x => x.HasValue(alias));
+
+            if (node == null)
+                return default;
+
+            return node.Value<T>(alias);
+        }
+    }
+
+}
